af9691c31eaabb84285ba34ca81544f1bc504ed0
Sucirahmarosa <ochasuci24@gmail.com>
2021-12-03 16:32:31 +0700

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author DELL
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+         System.out.println("1. Name, Suci Rahma Rosa");
+          System.out.println("2. Job, Website Development");
+           System.out.println("3. Freelance, Journalist KRadio Poltekpos");
+            System.out.println("4. Profile, I am an efficient and confident website developer with proven experience as coordinator of the K-Radio Journalists division of Pos\n" +
+                                "Indonesia Polytechnic. Other experiences really helped me develop\n" +
+                                "my knowledge and improve my skills to become a reliable website developer. I am very interested in continuing to learn\n" +
+                                "and improve my skills.");
+             System.out.println("5. Education, "
+                                 + "- Politeknik Pos Indonesia, D3 Teknik Informatika,\n" +
+                                   "GPA 3.80, 2021-2023\n" +
+                                   "- MAS Persis 24, IPA, 2017-2020");
+              System.out.println("6. Organization and Volunteer experience, "
+                                  + "- 2019: 1 Year - Bidgar Dakwah - RG-UG PPI (equal to Osis)\n" +
+                                  "- 2020: 2 weeks - Koor Pendidikan - Plkj PPI (equal to KKN)\n" +
+                                  "- 2021: 1 Year - Koor Jurnalis – K Radio Poltekpos \n" +
+                                  "- 2021: 3 months - Ketua Pelaksana Open recruitment K-Radio Poltekpos");
+               System.out.println("7. Achievement, - 2019: Top 20 selected broadcasters - national poetry contest – Mandiri Jaya publisher\n" +
+                                  "- 2019: Role model student - aliyyah level – PPI 24 Rancaekek\n" +
+                                  "- 2021: Finalis Radio Announcer Competition Comminfest 2021 – Universitas Atma Jaya Yogyakarta\n" +
+                                  "- 2021: Pandu Digital bradge Red - Kominfo");
+                System.out.println("8. Project, - Website Penyemangat.Yok\n" +
+                                  "- Website KRadio Poltekpos");
+                 System.out.println("9. Language, Indonesian language as the everyday language and ability to speak English passive skill levels");
+                  System.out.println("10. Skill, - Microsoft Office\n" +
+                                  "- React JS\n" +
+                                  "- Html\n" +
+                                  "- CSS\n" +
+                                  "- Javascript\n" +
+                                  "- MySQL server");
+                   System.out.println("11. Hobby, Run and Read");
+                    System.out.println("12. Contact, - Alamat, Jl.Cikopo Ds.Babakan Peuteuy RT/RW.004/003 Kec.Cicalengka Kab.Bandung\n" + 
+                            "- Gmail, Sucirr1@gmail.com\n" + "- No.Hp, 089509097798\n" + "- Linkedin, Suci Rahma Rosa\n");
+    }
+}
